4d3c5d8537db30a3edea298246e3fab2dd5b23be
31parkjulianTehCoder <105089696+31parkjulianTehCoder@users.noreply.github.com>
2026-02-10 13:04:06 -0800

    Update extentionListing-UI.js

--- tools/extentionListing-UI.js
+++ tools/extentionListing-UI.js
@@ -2,6 +2,9 @@ make.setTarget(null);
 make.h1(Info.name);
 make.h3("By " + Info.creator);
 make.p(Info.desc);
+if (isUnsandboxed === true) {
+    make.div("This extension needs to run unsandboxed. TurboWarp doesn't allow loading unsandboxed extensions from a URL, so this extension needs to be loaded from file or text to function properly. Also, when loading the extension, make sure to check 'Run Unsandboxed.'", { styles: " color: red; "})
+}
 make.div("", { id: "div-container", styles:" display: flex; justify-content: space-between; width: 190px;" });
 make.setTarget($("div-container"));
 make.button("Get", { id: "atlas-get" });
