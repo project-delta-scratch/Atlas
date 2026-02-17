22e0c4eef4c7e00a04d54833ebc778c6868e2185
31parkjulianTehCoder <105089696+31parkjulianTehCoder@users.noreply.github.com>
2026-02-17 11:44:09 -0800

    Update navbar-UI.js

--- tools/navbar-UI.js
+++ tools/navbar-UI.js
@@ -4,4 +4,4 @@ make.h2("Atlas")
 make.a("OSes", { href: "/TheAtlasStore/OSes/" });
 make.a("Apps", { href: "/TheAtlasStore/apps/" });
 make.a("Extensions", { href: "/TheAtlasStore/extentions/" });
-make.a("Blog", { href: "/TheAtlasStore/negative410.html" });
+make.a("Blog", { href: "/TheAtlasStore/blog/" });
