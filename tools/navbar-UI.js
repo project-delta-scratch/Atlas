0eacb38a28dd283d1c62324ff7bbb89798a805ed
31parkjulianTehCoder <105089696+31parkjulianTehCoder@users.noreply.github.com>
2026-06-22 00:58:36 +0900

    checks be slo frfr

--- tools/navbar-UI.js
+++ tools/navbar-UI.js
@@ -1,7 +1,7 @@
 make.div("", { id: "navbar", styles: " height: 50px; background-color: lightgray; display: flex; justify-content: space-between;" });
 make.setTarget($("navbar"));
 make.h2("Atlas")
-make.a("OSes", { href: "./OSes/" });
-make.a("Apps", { href: "./apps/" });
-make.a("Extensions", { href: "./extentions/" });
-make.a("Blog", { href: "./blog/" });
+make.a("OSes", { href: "../OSes/" });
+make.a("Apps", { href: "../apps/" });
+make.a("Extensions", { href: "../extentions/" });
+make.a("Blog", { href: "../blog/" });
